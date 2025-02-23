da6ff9c781f2a004c55445e9a3ef8e0f58b6541a
RayanHirari <rayan.hirari@gmail.com>
2025-02-23 16:56:18 +0100

    Create 1 - Lire les donnees.py
    Il y a ma fonction je l'ai fait y'a longtemps.
    Elle supprime les lignes dupliques, les lignes [,,,,,], les lignes d'en-tete ("Order Id, "Product" etc...)

---
+++ 1 - Lire les donnees.py
@@ -0,0 +1,31 @@
+def lire_csv(nom_fichier):
+    
+    """
+    Cette fonction ouvre un fichier csv et renvoie les donnees contenu dans le csv.
+
+    Parametres:
+    nom_fichier(str): le fichier sous format "Sales_April_2019.csv" par exemple
+    Renvoi / Return:
+    mon_csv_manipulable(list): liste de liste organisee avec, pour chaque sous liste(=chaque ligne du csv), 
+                              Order ID;Product;Quantity Ordered;Price Each;Order Date;Purchase Address). On peut la modifier dans la suite de l'exo.
+    """
+    
+    #ouvrir.
+    with open(nom_fichier, newline='') as fichier_csv:
+        #lire et stocker dans une variable.
+        mon_csv = csv.reader(fichier_csv, delimiter=',') #le delimiter sert a identifier quel caractere separe les elements de la ligne.
+        #transformer en une liste de liste
+        mon_csv = list(mon_csv)
+
+
+    #je veux des valeurs distincts
+    mon_csv_manipulable = []
+    for liste in mon_csv:
+        if liste not in mon_csv_manipulable:
+            mon_csv_manipulable.append(liste)
+    
+    #supprimer les lignes inutiles
+    mon_csv_manipulable.pop(mon_csv_manipulable.index(['', '', '', '', '', '']))
+    mon_csv_manipulable.pop(mon_csv_manipulable.index(['Order ID', 'Product', 'Quantity Ordered', 'Price Each', 'Order Date', 'Purchase Address']))
+
+    return mon_csv_manipulable
